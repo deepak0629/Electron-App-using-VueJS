7e21097a460bb9cdaa7dd9b82de346c9c6f7ef39
deepak <deepak06299@gmail.com>
2021-07-21 02:37:41 -0400

    TO-DO App

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'temp-vue-project-with-electron',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
